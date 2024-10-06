1974f3852a10b0a0a259e36c4d8a8ac01b7e7b8c
padilhafe <felipedpadilha@gmail.com>
2024-10-06 14:46:29 -0300

    first commit

---
+++ fastapi/books.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Body
+
+app = FastAPI()
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+BOOKS = [
+    Book(1, "Computer Science Pro", "codingwithroby", "A very nice book!", 5),
+    Book(2, "Be fast with FastAPI", "codingwithroby", "This is a great book!", 5),
+    Book(3, "Master Endpoints", "codingwithroby", "Awesome book!", 5),
+    Book(4, "HP1", "Author 1", "Book Description", 2),
+    Book(5, "HP2", "Author 2", "Book Description", 3),
+    Book(6, "HP3", "Author 3", "Book Description", 1)
+]
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+@app.post("/create-book")
+async def create_book(book_request = Body()):
+    BOOKS.append(book_request)
